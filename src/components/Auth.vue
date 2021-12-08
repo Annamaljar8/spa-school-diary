8bc48a62241775c900e98eac8ba7a55b25338870
Oleksandra Kharytonova <oleksandra.kharytonova@execon.pl>
2021-12-08 14:58:28 +0100

    add logOut() and getUser() in Auth.vue; add router in About.vue

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -19,7 +19,18 @@
     >
       submit
     </v-btn>
-   
+    <v-btn
+      class="mr-4"
+      @click="logOut"
+    >
+      log out
+    </v-btn>
+    <v-btn
+      class="mr-4"
+      @click="getUser"
+    >
+      get user
+    </v-btn>
   </form>
 </template>
 
@@ -34,10 +45,31 @@ import axios from 'axios';
     }),
     methods: {
       submit(){
+        let self = this;
         axios.post('/login', {
           email: this.email,
           password: this.password
         })
+        .then(function (response) {
+          console.log(response);
+          self.$router.push('about')
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      },
+      logOut(){
+        axios.post('/logout', {
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      },
+      getUser(){
+        axios.get('/user')
         .then(function (response) {
           console.log(response);
         })
